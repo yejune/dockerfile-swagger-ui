73ebc758e9a57e420014e285a9a0f2ba07a57df5
yejune <smkwon@yellotravel.com>
2017-02-03 18:50:44 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,6 +10,14 @@ if [ ! -z "$API_URL" ] ; then
   sed -i "s|http://example.com/api|${API_URL}|g" index.html
 fi
 
+sed -i "s|.*scheme:.*$||g" index.html
+
+if [ ! -z "$USE_HTTPS" ]; then
+  sed -i "s|\(url: url,.*\)|\1\n        scheme: \"https\",|g" index.html
+else
+  sed -i "s|\(url: url,.*\)|\1\n        scheme: \"http\",|g" index.html
+fi
+
 sed -i "s|.*validatorUrl:.*$||g" index.html
 
 if [ ! -z "$VALIDATOR_URL" ]; then
